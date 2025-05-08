3af9f70c8ff4f6a175868ed6fc300e988f10cd84
komalgc <komalgc17@gmail.com>
2025-05-07 18:03:32 -0600

    pageobjects

--- Playwright/test_framework_web_api.py
+++ Playwright/test_framework_web_api.py
@@ -3,6 +3,8 @@ import json
 import pytest
 from playwright.sync_api import Playwright, expect
 
+from Playwright.PageObjects.dashboardpage import DashboardPage
+from Playwright.PageObjects.loginpage import LoginPage
 from utils.apibase import APIBase
 
 # Json-->util-->access to test
@@ -13,6 +15,10 @@ with open('data/credentials.json') as f:
 
 @pytest.mark.parametrize('user_credentials',user_credential_list)
 def test_webapi(playwright:Playwright, user_credentials):
+    useremail = user_credentials["userEmail"]
+    userpassword = user_credentials["userPassword"]
+
+
     browser=playwright.chromium.launch(headless=False)
     context=browser.new_context()
     page=context.new_page()
@@ -21,12 +27,15 @@ def test_webapi(playwright:Playwright, user_credentials):
     apiutils = APIBase()
     orderId= apiutils.createorder(playwright, user_credentials)
 
-    #login
-    page.goto("https://rahulshettyacademy.com/client/")
-    page.get_by_placeholder("email@example.com").fill(user_credentials["userEmail"])
-    page.get_by_placeholder("enter your passsword").fill(user_credentials["userPassword"])
-    page.get_by_role("button", name="Login").click()
-    page.get_by_role("button", name="ORDERS").click()
+
+    #loginpage
+    loginPage =LoginPage(page)
+    loginPage.navigate()
+    loginPage.login(useremail,userpassword)
+
+    #dashboardpage
+    dashboard = DashboardPage(page)
+    dashboard.selectOrdersNavLink()
 
     #orders history page -> order is present
     row = page.locator("tr").filter(has_text=orderId)
