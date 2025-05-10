30375f7132d49686ecd13de7d4719fd52967ca44
komalgc <komalgc17@gmail.com>
2025-05-09 22:28:37 -0600

    testing

--- Playwright/conftest.py
+++ Playwright/conftest.py
@@ -4,3 +4,12 @@ import pytest
 @pytest.fixture(scope="session")
 def user_credentials(request):
     return request.param
+
+
+@pytest.fixture(scope="session")
+def browserInstance(playwright,request):
+    browser_name = request.config.getoption("browser_name")
+    browser=playwright.chromium.launch(headless=False)
+    context=browser.new_context()
+    page=context.new_page()
+
